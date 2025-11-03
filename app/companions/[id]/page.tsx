ddd22d7cba7967875674d9c230762ab6458cb6d5
Adarsh Pathak <pathakadarsh720@gmail.com>
2025-11-04 03:13:52 +0530

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompanionSession() {
+  return (
+    <div>
+      CompanionSession
+    </div>
+  )
+}
+
+export default CompanionSession
---
+++ app/companions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompanionsLibrary() {
+  return (
+    <div>
+      CompanionsLibrary
+    </div>
+  )
+}
+
+export default CompanionsLibrary
