64f486ca5839586e2af2c6ecb33e126376898a96
Adarsh Pathak <pathakadarsh720@gmail.com>
2025-11-05 16:11:49 +0530

    final commit

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -24,8 +24,8 @@ const NewCompanion= async()=>{
           <div className="cta-badge">
             Upgrade your plan
           </div>
-          <h1>You’ve Reached Your Limit</h1>
-          <p>You’ve reached your companion limit. Upgrade to create more companions and premium features.</p>
+          <h1>You've Reached Your Limit</h1>
+          <p>You've reached your companion limit. Upgrade to create more companions and premium features.</p>
           <Link href="/subscription" className="btn-primary w-full justify-center" >
             Upgrade My Plan
           </Link>
